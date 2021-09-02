22db738497dba396b5b88419c00a0f713beb18f2
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-02 17:26:33 +0300

    0.1
    Залил 3(5) строк кода, юхху

--- Hunter_Summons_warlock.py
+++ Hunter_Summons_warlock.py
@@ -1,1 +1,5 @@
+class Hunter(Unit): # summoner sniper (chto ya delayu voobche)
 
+class Hunter_summons(Unit):
+        
+class Warlock(Unit): # мазохист dd (Много, очень очень много) (can heal himself)
