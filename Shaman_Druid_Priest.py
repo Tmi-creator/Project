b0a85e22775e7ac4aad4ef38d522a420e9d5d419
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-02 17:29:09 +0300

    0.1
    1\3 классов

--- Shaman_Druid_Priest.py
+++ Shaman_Druid_Priest.py
@@ -1,1 +1,43 @@
+class Shaman(Unit): # totem dd healer
+    
+class Druid(Unit): # transformer dd healer
+
+class Priest(Unit): # super healer (can revive)
+   
+    def heal(target):
+        if (self.mana >= 10):
+            target.hp += atk * first_skill
+            if (target.hp > target.max_hp):
+                target.hp = target.max_hp
+            self.mana -= 10
+        # + buff first_skill готово
+        else:
+            print('No mana!')
+            target.takeDamage(self.atk * 0.5)
+
+    def holy_light(target):
+        if (self.mana >= 10):
+            self.mana -= 10
+            if (target.third_skill == 'CREATURE'):
+                target.hp - 1000
+            else:
+                target.take_damage(self.atk * 0.5)
+                target.third_skill -= second_skill
+                # добавить дебафф second_skill готово
+                
+        else:
+            print('No mana!')
+            target.take_damage(self.atk * 0.5)
+
+
+    def revive(target):
+        if (self.mana >= 10):
+            target.max_hp+=self.third_skill
+            if (target.hp <= 0):
+                target.hp = target.max_hp * 0.5
+            else:
+                target.hp = target.max_hp
+        else:
+            print('No mana!')
+            target.take_damage(self.atk * 0.5)
 
