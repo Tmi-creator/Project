a43f67d69c9b50375918c5b0f36a905fcdbd0814
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-02 17:21:33 +0300

    0.1
    Залил юнита (водой)

--- Unit.py
+++ Unit.py
@@ -1,1 +1,17 @@
+class Unit(object):
+    def __init__(self, hp, atk, mana, first_skill, second_skill, third_skill):
+        self.hp = hp
+        self.max_hp = hp
+        self.atk = atk
+        self.cur_atk = atk
+        self.mana = mana
+        self.first_skill = first_skill
+        self.second_skill = second_skill
+        self.third_skill = third_skill
+
+    def take_damage(dmg):
+        self.hp -= dmg
+
+    def attack(target):
+        target.take_damage(self.atk)
 
