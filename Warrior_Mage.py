38f4071eb3f9cc1cfd3145548f71fec5985edd40
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-03 18:55:12 +0300

    refactor 0.2

--- Warrior_Mage.py
+++ Warrior_Mage.py
@@ -1,35 +1,38 @@
 from random import randint
-class Warrior(Unit): # armor dd
+
+
+class Warrior(Unit):  # armor dd
     def take_damage(dmg):
         self.hp -= dmg + first_skill
 
-    def simple_skill(target): #
-        if(self.mana >= 10):
+    def simple_skill(target):  #
+        if (self.mana >= 10):
             target.take_damage(self.second_skill + self.atk * 0.5)
             self.mana -= 10
 
-    def kill_rogue(target): #
-        if(self.mana >= 10):
-            if(target == Rogue):
+    def kill_rogue(target):  #
+        if (self.mana >= 10):
+            if (target == Rogue):
                 target.take_damaged(self.third_skill)
                 target.take_damaged(self.third_skill)
                 self.mana -= 10
 
-class Mage(Unit): # super dd
+
+class Mage(Unit):  # super dd
     def total_annigilation(target):
-        if(self.mana >= 10):
-            target.take_damage(self.first_skill * randint(0,1) * randint(0,1) * randint(0,1) * randint(0,1) * randint(0,1))
+        if (self.mana >= 10):
+            target.take_damage(
+                self.first_skill * randint(0, 1) * randint(0, 1) * randint(0, 1) * randint(0, 1) * randint(0, 1))
             self.mana -= 10
 
     def buff(target):
-        if(self.mana >= 10):
+        if (self.mana >= 10):
             target.atk += second_skill
             self.mana -= 10
 
     def mana_up(target):
-        if(self.mana >= 10):
-            target.mana += third_skill-10
+        if (self.mana >= 10):
+            target.mana += third_skill - 10
         else:
             print('No mana!')
             target.take_damage(self.atk * 0.5)
-
