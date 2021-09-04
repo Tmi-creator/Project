8d4bdf28ecd592105b4b7c6b1ab1a545788b0d3b
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-04 17:07:43 +0300

    0.3.1

--- Hunter_Summons_warlock.py
+++ Hunter_Summons_warlock.py
@@ -1,7 +1,7 @@
 class Hunter(Unit): # summoner sniper (chto ya delayu voobche)
-
+        pass
 class Hunter_summons(Unit):
-        
+        pass
 class Warlock(Unit): # мазохист dd (Много, очень очень много) (can heal himself)
         self.hp=150
         self.atk=20
