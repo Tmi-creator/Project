c62960385a2b4f0ea5c9762e8686c879ce12b0a8
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-04 16:36:24 +0300

    0.3
    сразу, без рефакторинга, в 0.3

--- Hunter_Summons_warlock.py
+++ Hunter_Summons_warlock.py
@@ -3,3 +3,6 @@ class Hunter(Unit): # summoner sniper (chto ya delayu voobche)
 class Hunter_summons(Unit):
         
 class Warlock(Unit): # мазохист dd (Много, очень очень много) (can heal himself)
+        self.hp=150
+        self.atk=20
+        self.mana=200
