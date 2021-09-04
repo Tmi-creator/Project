4247f5364dcf171ce3b37f61ff3d36d9a2c97eaa
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-04 16:35:01 +0300

    0.3
    Как и в остальных классах

--- Shaman_Druid_Priest.py
+++ Shaman_Druid_Priest.py
@@ -5,39 +5,48 @@ class Druid(Unit):  # transformer dd healer
 
 
 class Priest(Unit):  # super healer (can revive)
-
-    def heal(target):
-        if (self.mana >= 10):
-            target.hp += atk * first_skill
-            if (target.hp > target.max_hp):
-                target.hp = target.max_hp
-            self.mana -= 10
-        # + buff first_skill готово
-        else:
-            print('No mana!')
-            target.takeDamage(self.atk * 0.5)
-
-    def holy_light(target):
-        if (self.mana >= 10):
-            self.mana -= 10
-            if (target.third_skill == 'CREATURE'):
-                target.hp - 1000
+    self.hp=150
+    self.atk=20
+    self.mana=50
+    self.first_skill=80
+    self.second_skill=5
+    self.third_skill=5
+    
+    def first_skill():
+        def heal(target):
+            if (self.mana >= 10):
+                target.hp += atk * first_skill
+                if (target.hp > target.max_hp):
+                    target.hp = target.max_hp
+                self.mana -= 10
+            # + buff first_skill готово
             else:
+                print('No mana!')
+                target.takeDamage(self.atk * 0.5)
+    
+    def second_skill():
+        def holy_light(target):
+            if (self.mana >= 10):
+                self.mana -= 10
+                if (target.third_skill == 'CREATURE'):
+                    target.hp - 1000
+                else:
+                    target.take_damage(self.atk * 0.5)
+                    target.third_skill -= second_skill
+                    # добавить дебафф second_skill готово
+
+            else:
+                print('No mana!')
                 target.take_damage(self.atk * 0.5)
-                target.third_skill -= second_skill
-                # добавить дебафф second_skill готово
 
-        else:
-            print('No mana!')
-            target.take_damage(self.atk * 0.5)
-
-    def revive(target):
-        if (self.mana >= 10):
-            target.max_hp += self.third_skill
-            if (target.hp <= 0):
-                target.hp = target.max_hp * 0.5
+    def third_skill():
+        def revive(target):
+            if (self.mana >= 10):
+                target.max_hp += self.third_skill
+                if (target.hp <= 0):
+                    target.hp = target.max_hp * 0.5
+                else:
+                    target.hp = target.max_hp
             else:
-                target.hp = target.max_hp
-        else:
-            print('No mana!')
-            target.take_damage(self.atk * 0.5)
+                print('No mana!')
+                target.take_damage(self.atk * 0.5)
