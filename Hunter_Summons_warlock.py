79fbb6c6270aaf629aa9f70c2bbb69186b9f5b09
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-09 14:55:01 +0300

    4.0.1

--- Hunter_Summons_warlock.py
+++ Hunter_Summons_warlock.py
@@ -1,9 +1,28 @@
  __init__.py
 class Hunter(Unit): # summoner sniper (chto ya delayu voobche)
+        skills = {
+        1:self.attack()
+        2:self.first_skill()
+        3:self.second_skill()
+        4:self.third_skill()
+        }
         pass
 class Hunter_summons(Unit):
+        skills = {
+        1:self.attack()
+        2:self.first_skill()
+        3:self.second_skill()
+        4:self.third_skill()
+        }
         pass
 class Warlock(Unit): # мазохист dd (Много, очень очень много) (can heal himself)
         self.hp=150
         self.atk=20
         self.mana=200
+        
+        skills = {
+        1:self.attack()
+        2:self.first_skill()
+        3:self.second_skill()
+        4:self.third_skill()
+        }
