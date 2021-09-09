10484041a83725ba2fc905ba277917ecc2ccc20d
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-09 14:32:38 +0300

    0.3.2.1

--- Warrior_Mage.py
+++ Warrior_Mage.py
@@ -5,26 +5,26 @@ class Warrior(Unit):  # armor dd
     self.hp=200
     self.atk=20
     self.mana=50
-    self.first_skill=-10
-    self.second_skill=40
-    self.third_skill=15
+    self.first_skill_num=-10
+    self.second_skill_num=40
+    self.third_skill_num=15
     
     def first_skill():
         def take_damage(dmg):
-            self.hp -= dmg + first_skill
+            self.hp -= dmg + first_skill_num
             
     def second_skill():
         def simple_skill(target):  #
             if (self.mana >= 10):
-                target.take_damage(self.second_skill + self.atk * 0.5)
+                target.take_damage(self.second_skill_num + self.atk * 0.5)
                 self.mana -= 10
 
     def third_skill():
         def kill_rogue(target):  #
             if (self.mana >= 10):
                 if (target == Rogue):
-                    target.take_damaged(self.third_skill)
-                    target.take_damaged(self.third_skill)
+                    target.take_damaged(self.third_skill_num)
+                    target.take_damaged(self.third_skill_num)
                     self.mana -= 10
 
 
@@ -32,27 +32,27 @@ class Mage(Unit):  # super dd
     self.hp=100
     self.atk=40
     self.mana=100
-    self.first_skill=2048
-    self.second_skill=10
-    self.third_skill=15
+    self.first_skill_num=2048
+    self.second_skill_num=10
+    self.third_skill_num=15
     
     def first_skill():
         def total_annigilation(target):
             if (self.mana >= 10):
                 target.take_damage(
-                    self.first_skill * randint(0, 1) * randint(0, 1) * randint(0, 1) * randint(0, 1) * randint(0, 1))
+                    self.first_skill_num * randint(0, 1) * randint(0, 1) * randint(0, 1) * randint(0, 1) * randint(0, 1))
                 self.mana -= 10
 
     def second_skill():
         def buff(target):
             if (self.mana >= 10):
-                target.atk += second_skill
+                target.atk += second_skill_num
                 self.mana -= 10
 
     def third_skill():
         def mana_up(target):
             if (self.mana >= 10):
-                target.mana += third_skill - 10
+                target.mana += third_skill_num - 10
             else:
                 print('No mana!')
                 target.take_damage(self.atk * 0.5)
