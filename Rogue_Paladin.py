9e9f2dfb6e11fe533c3f5e08a5094642544b711b
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-09 14:36:39 +0300

    0.3.2.1

--- Rogue_Paladin.py
+++ Rogue_Paladin.py
@@ -11,13 +11,13 @@ class Rogue(Unit):
     
     def first_skill():
         def take_damage(dmg):
-            if (randint(1, 100) > self.first_skill):
+            if (randint(1, 100) > self.first_skill_num):
                 self.hp -= dmg
             else:
                 print('Dodged:)')
 
         def take_damaged(dmg):
-            if (randint(1, 100) > self.first_skill // 3):
+            if (randint(1, 100) > self.first_skill_num // 3):
                 self.hp -= dmg
             else:
                 print('Dodged :)')
@@ -25,7 +25,7 @@ class Rogue(Unit):
     def second_skill():
         def blades_of_death(target):
             if (self.mana >= 10):
-                if (randint(1, 100) > self.second_skill):
+                if (randint(1, 100) > self.second_skill_num):
                     attack(target)
                     attack(target)
                 else:
@@ -38,7 +38,7 @@ class Rogue(Unit):
     def third_skill():
         def no_weapon(target):
             if (self.mana >= 10):
-                target.cur_atk -= randint(1, self.third_skill)
+                target.cur_atk -= randint(1, self.third_skill_num)
                 self.mana -= 10
             else:
                 print('No mana!')
@@ -55,12 +55,12 @@ class Paladin(Unit):  # armor healer
     
     def first_skill():
         def take_damage(dmg):
-            self.hp -= dmg + first_skill
+            self.hp -= dmg + first_skill_num
 
     def second_skill():
         def heal(target):
             if (self.mana >= 10):
-                target.hp += second_skill
+                target.hp += second_skill_num
                 if (target.hp > target.max_hp):
                     target.hp = target.max_hp
                 self.mana -= 10
@@ -70,6 +70,6 @@ class Paladin(Unit):  # armor healer
 
     def third_skill():
         def help_of_god(target):
-            target.first_skill += self.third_skill
-            target.second_skill += self.third_skill
+            target.first_skill_num += self.third_skill_num
+            target.second_skill_num += self.third_skill_num
             self.mana -= 10 * 3
