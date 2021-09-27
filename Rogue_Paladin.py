45d73193def5a13dd7700ab78fea3f700d77eefc
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-27 18:04:19 +0300

    0.6

--- Rogue_Paladin.py
+++ Rogue_Paladin.py
@@ -1,48 +1,53 @@
 from random import randint
+
+import self
+
 from Unit import Unit
 
+
 class Rogue(Unit):
-    self.hp=50
-    self.atk=60
-    self.mana=50
-    self.first_skill_num=75
-    self.second_skill_num=50
-    self.third_skill_num=10
-    skills = {
-        1:self.attack()
-        2:self.first_skill()
-        3:self.second_skill()
-        4:self.third_skill()
+    self.hp = 50
+    self.atk = 60
+    self.mana = 50
+    self.first_skill_num = 75
+    self.second_skill_num = 50
+    self.third_skill_num = 10
+    self.skills = {
+        1: self.attack,
+        2: self.first_skill,
+        3: self.second_skill,
+        4: self.third_skill
     }
-    def first_skill():
-        def take_damage(dmg):
-            if (randint(1, 100) > self.first_skill_num):
+
+    def first_skill(dmg):
+        def take_damage():
+            if randint(1, 100) > self.first_skill_num:
                 self.hp -= dmg
             else:
                 print('Dodged:)')
 
         def take_damaged(dmg):
-            if (randint(1, 100) > self.first_skill_num // 3):
+            if randint(1, 100) > self.first_skill_num // 3:
                 self.hp -= dmg
             else:
                 print('Dodged :)')
 
-    def second_skill():
-        def blades_of_death(target):
-            if (self.mana >= 10):
-                if (randint(1, 100) > self.second_skill_num):
-                    attack(target)
-                    attack(target)
+    def second_skill(target):
+        def blades_of_death():
+            if self.mana >= 10:
+                if randint(1, 100) > self.second_skill_num:
+                    self.attack(target)
+                    self.attack(target)
                 else:
-                    attack(target)
+                    self.attack(target)
                 self.mana -= 10
             else:
                 print('No mana!')
                 target.take_damage(self.atk * 0.5)
-    
-    def third_skill():
-        def no_weapon(target):
-            if (self.mana >= 10):
+
+    def third_skill(target):
+        def no_weapon():
+            if self.mana >= 10:
                 target.cur_atk -= randint(1, self.third_skill_num)
                 self.mana -= 10
             else:
@@ -51,26 +56,27 @@ class Rogue(Unit):
 
 
 class Paladin(Unit):  # armor healer
-    self.hp=150
-    self.atk=15
-    self.mana=75
-    self.first_skill_num=-5
-    self.second_skill_num=40
-    self.third_skill_num=10
-    skills = {
-        1:self.attack()
-        2:self.first_skill()
-        3:self.second_skill()
-        4:self.third_skill()
+    self.hp = 150
+    self.atk = 15
+    self.mana = 75
+    self.first_skill_num = -5
+    self.second_skill_num = 40
+    self.third_skill_num = 10
+    self.skills = {
+        1: self.attack,
+        2: self.first_skill,
+        3: self.second_skill,
+        4: self.third_skill
     }
-    def first_skill():
-        def take_damage(dmg):
-            self.hp -= dmg + first_skill_num
 
-    def second_skill():
-        def heal(target):
-            if (self.mana >= 10):
-                target.hp += second_skill_num
+    def first_skill(dmg):
+        def take_damage():
+            self.hp -= dmg + self.first_skill_num
+
+    def second_skill(target):
+        def heal():
+            if self.mana >= 10:
+                target.hp += self.second_skill_num
                 if (target.hp > target.max_hp):
                     target.hp = target.max_hp
                 self.mana -= 10
@@ -78,8 +84,8 @@ class Paladin(Unit):  # armor healer
                 print('No mana!')
                 target.take_damage(self.atk * 0.5)
 
-    def third_skill():
-        def help_of_god(target):
+    def third_skill(target):
+        def help_of_god():
             target.first_skill_num += self.third_skill_num
             target.second_skill_num += self.third_skill_num
             self.mana -= 10 * 3
