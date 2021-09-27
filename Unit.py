3693364c1d525ff8f6c8de7f115305559d47d823
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-27 18:05:10 +0300

    0.6

--- Unit.py
+++ Unit.py
@@ -1,5 +1,8 @@
+import self as self
+
+
 class Unit(object):
-    def __init__(self, hp, atk, mana, first_skill, second_skill, third_skill):
+    def __init__(self, hp, atk, mana, first_skill_num, second_skill_num, third_skill_num):
         self.hp = hp
         self.max_hp = hp
         self.atk = atk
@@ -8,24 +11,27 @@ class Unit(object):
         self.first_skill_num = first_skill_num
         self.second_skill_num = second_skill_num
         self.third_skill_num = third_skill_num
-    
-    skills = {
-        1:self.attack()
-        2:self.first_skill()
-        3:self.second_skill()
-        4:self.third_skill()
-    }
+        self.skills = {
+            1: self.attack,
+            2: self.first_skill,
+            3: self.second_skill,
+            4: self.third_skill
+        }
+
+
     def take_damage(dmg):
         self.hp -= dmg
 
     def attack(target):
         target.take_damage(self.atk)
-        
-    def first_skill():
-         raise Exception("Override me")
-            
-    def second_skill():
-         raise Exception("Override me")
-            
-    def third_skill():
-         raise Exception("Override me")
+
+    def first_skill(target):
+        raise Exception("Override me")
+
+    def second_skill(target):
+        raise Exception("Override me")
+
+    def third_skill(target):
+        raise Exception("Override me")
+
+
