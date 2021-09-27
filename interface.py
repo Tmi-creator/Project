a5a255341c9cca9477f4a25d4c7366c4b91c84ff
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2021-09-27 18:06:03 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -3,15 +3,18 @@ from Rogue_Paladin import Rogue, Paladin
 from Shaman_Druid_Priest import Druid, Priest
 from Unit import Unit
 from Warrior_Mage import Warrior, Mage
+
+
 def interface():
     print("Приветствую тебя, странник! Если желаешь сыграть в пвп, жми 1, если против мобов - 2.")
     choice1 = int(input())
-    if (choice1 == 1):
+    if choice1 == 1:
         print("1 на 1(1), командное(2) или ффа(3)?")
         choice2 = int(input())
-        if (choice2 == 1):
+        if choice2 == 1:
             print(
-                "Ну, да начнется бой!\nВыберете персонажа для битвы:\nВоин-1\nМаг-2\nРазбойник-3\nПаладин-4\nОхотник-5\nЧернокнижник-6\nШаман-7\nДруид-8\nЖрец-9\n")
+                "Ну, да начнется бой!\nВыберете персонажа для "
+                "битвы:\nВоин-1\nМаг-2\nРазбойник-3\nПаладин-4\nОхотник-5\nЧернокнижник-6\nШаман-7\nДруид-8\nЖрец-9\n")
             choice3 = int(input())
 
             choice4 = int(input())
@@ -21,9 +24,9 @@ def interface():
                 2: Mage,
                 3: Rogue,
                 4: Paladin,
-                5: Hunter,
+                #5: Hunter,
                 6: Warlock,
-                7: Shaman,
+                #7: Shaman,
                 8: Druid,
                 9: Priest
 
@@ -41,20 +44,21 @@ def interface():
                 print(p1.hp, p1.cur_atk, p1.mana, "- p1")
                 print(p2.hp, p2.cur_atk, p2.mana, "- p2")
                 print(
-                    "p1 turn, 1 - обычная атака,\n 2 - использование 1 способности,\n 3 - использование второй способности, и\n 4 - использование 3 способности\n")
-                
+                    "p1 turn, 1 - обычная атака,\n 2 - использование 1 способности,\n 3 - использование второй "
+                    "способности, и\n 4 - использование 3 способности\n")
+
                 choice = int(input())
                 while choice not in p1.skills:
                     print("Incorrect choice.")
                     choice = int(input())
                 t1 = int(input())
-                p1.skills[choice]
+                p1.skills[choice](p2)
                 choice = int(input())
                 while choice not in p2.skills:
                     print("Incorrect choice.")
                     choice = int(input())
                 t2 = int(input())
-                p2.skills[choice]
-            
-if (__name__ == __main__):
-    run.interface()
+                p2.skills[choice](p1)
+
+
+interface()
